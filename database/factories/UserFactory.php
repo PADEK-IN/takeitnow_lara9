fe558977a2b506456834d48ebf1ba60d6f07cb11
fajrCode <fajri42vandi@gmail.com>
2024-06-23 07:58:32 +0700

    feat: auth -> breeze

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,9 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -18,11 +19,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'), // password
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'name' => $this->faker->name(),
+            'gender' => "male",
+            'phone' => $this->faker->e164PhoneNumber(),
         ];
     }
 
@@ -38,5 +40,20 @@ class UserFactory extends Factory
                 'email_verified_at' => null,
             ];
         });
+    }
+
+    public function female(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'name' => $this->faker->name("female"),
+            'gender' => "female",
+        ]);
+    }
+
+    public function setAdmin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => "admin",
+        ]);
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('name', 50);
+            $table->enum('gender', ['female', 'male']);
+            $table->string('phone', 17);
+            $table->string('image', 150)->default('user.png');
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
