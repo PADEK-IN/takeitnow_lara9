3d38cdf3bdcc5a58d23a2121ed5f8e777e80b836
fajrCode <fajri42vandi@gmail.com>
2024-06-24 09:37:21 +0700

    fix: transaction qty

--- app/Http/Controllers/admin/IndexController.php
+++ app/Http/Controllers/admin/IndexController.php
@@ -18,7 +18,11 @@ class IndexController extends Controller
         $qtyUser = User::count();
         $qtyEvent = Event::count();
         $qtyReview = Review::count();
-        $qtyTransaction = Transaction::all()->sum(function ($transaction) {
+        // $qtyTransaction = Transaction::where('isValid', true)
+        //                             ->join('events', 'transactions.id_event', '=', 'events.id')
+        //                             ->selectRaw('sum(events.price * transactions.quantity) as total_value')
+        //                             ->value('total_value');
+        $qtyTransaction = Transaction::with("eventData")->where("isValid", true)->get()->sum(function ($transaction) {
             $price = $transaction->eventData->price;
             $quantity = $transaction->quantity;
             return $price*$quantity;
@@ -30,23 +34,6 @@ class IndexController extends Controller
             'review' => $qtyReview,
             'transaction' => $qtyTransaction,
         ];
-
-        // $oneMonthAgo = Carbon::now()->subMonth();
-
-        // $transactions = Transaction::where('created_at', '>=', $oneMonthAgo)
-        //     ->with('eventData')
-        //     ->get()
-        //     ->groupBy(function($date) {
-        //         return Carbon::parse($date->created_at)->format('Y-m-d'); // grouping by days
-        //     });
-
-        // $dates = [];
-        // $data = [];
-
-        // foreach ($transactions as $date => $transactionsByDate) {
-        //     $dates[] = $date;
-        //     $data[] = $transactionsByDate->count();
-        // }
 
         // Mengirim dataQty ke view
         return view('pages.admin.dashboard.index', compact('qty'));
--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -4,7 +4,7 @@
         <!-- total views   -->
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card">
-                <div class="card-body">
+                <a href="/admin/user" class="card-body">
                     <div class="d-inline-block">
                         <h5 class="text-muted">Total Pengguna</h5>
                         <h3 class="mb-0">{{ $qty['user'] }}</h3>
@@ -12,14 +12,14 @@
                     <div class="float-right icon-circle-medium  icon-box-lg  bg-info-light mt-1">
                         <i class="fa fa-user fa-fw fa-sm text-info"></i>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
         <!-- end total views   -->
         <!-- total followers   -->
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card">
-                <div class="card-body">
+                <a href="/admin/event" class="card-body">
                     <div class="d-inline-block">
                         <h5 class="text-muted">Total Acara</h5>
                         <h3 class="mb-0">{{ $qty['event'] }}</h3>
@@ -27,14 +27,14 @@
                     <div class="float-right icon-circle-medium  icon-box-lg  bg-primary-light mt-1">
                         <i class="fa fa-eye fa-fw fa-sm text-primary"></i>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
         <!-- end total followers   -->
         <!-- partnerships   -->
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card">
-                <div class="card-body">
+                <a href="/admin/review" class="card-body">
                     <div class="d-inline-block">
                         <h5 class="text-muted">Total Reviews</h5>
                         <h3 class="mb-0">{{ $qty['review'] }}</h3>
@@ -42,22 +42,22 @@
                     <div class="float-right icon-circle-medium  icon-box-lg  bg-secondary-light mt-1">
                         <i class="fa fa-comments fa-fw fa-sm text-secondary"></i>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
         <!-- end partnerships   -->
         <!-- total earned   -->
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card">
-                <div class="card-body">
+                <a href="/admin/transaction" class="card-body">
                     <div class="d-inline-block">
-                        <h5 class="text-muted">Total Transaksi</h5>
+                        <h5 class="text-muted">Total Pendapatan</h5>
                         <h3 class="mb-0">{{ formatRupiah($qty['transaction']) }}</h3>
                     </div>
                     <div class="float-right icon-circle-medium  icon-box-lg  bg-brand-light mt-1">
                         <i class="fa fa-money-bill-alt fa-fw fa-sm text-brand"></i>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
         <!-- end total earned   -->
